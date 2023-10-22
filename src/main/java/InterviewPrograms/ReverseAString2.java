83c10fecf9864f7f7998d1a7483d2d6c200fab84
Rachit Chandra <rachitchandra220@gmail.com>
2023-10-22 14:04:04 +0530

    Updated

--- src/main/java/InterviewPrograms/ReverseAString2.java
+++ src/main/java/InterviewPrograms/ReverseAString2.java
@@ -1,7 +1,7 @@
 package InterviewPrograms;
 
 //Reverse a string
-public class BLR_0001 {
+public class ReverseAString2 {
 	public static void main(String args[]) {
 		String name="Rachit";
 		for(int i=name.length()-1;i>=0;i--) {
--- src/test/java/TestNG/ListnersClassExample.java
+++ src/test/java/TestNG/ListnersClassExample.java
@@ -2,9 +2,29 @@ package TestNG;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import org.testng.annotations.Listeners;
 
 public class ListnersClassExample implements ITestListener{
-
+	
+//	listens to every event that occurs in a selenium code. 
+//	Listeners are activated either before the test or after the test case
+//  ITestListener interface
+//	@Listeners annotation within the class AND Use within the testng.xml
+//	@Listeners(ListnersClassExample.class) in class
+	
+//	<suite name="Suite">  
+//	<listeners>  
+//	<listener class-name="com.javatpoint.Listener"/>  
+//	</listeners>  
+//	<test name="Listeners_program">  
+//	<classes>  
+//	<class name="com.javatpoint.Testcases"></class>  
+//	</classes>  
+//	</test>  
+//	</suite>   
+//	<!-- Suite -->  
+	
+	
 	public void onTestStart(ITestResult result) {
 		System.out.println("#####ListnersClassExample - OnTestStart#####");
 	}
